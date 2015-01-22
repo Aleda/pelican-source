12194a4926c07d1efbee76720d381c08330aabd7
Aleda <348983192@qq.com>
2015-01-22 14:16:41 +0800

    pelican default conf

--- content/HelloWorld.md
+++ content/HelloWorld.md
@@ -1,9 +1,20 @@
-Title: My super title
-Date: 2010-12-03 10:20
-Category: Python
-Tags: pelican, publishing
-Slug: my-super-post
-Author: Alexis Metaireau
+Title: 入住github
+Date: 2015-01-22 11:19
+Category: dailynotes
+Tags: dailynotes 
+Author: Aleda
 Summary: Short version for index and feeds
 
-This is the content of my super blog post.
+# 入住github
+
+---
+### 一、 随笔
+总算也是有点眉目了，整整花了一天加一早上的时间，真的是有点费事啊，下午必须开始高强度的学习了，
+要不真的完不成这周的任务了。
+
+### 二、 时间规划
+
+#### 2.1 上午
+> * 上午把这个pelican的配置文件学学。
+*时间要求：* **2h** |  *优先级：* `high` | *状态：* `running`
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -6,21 +6,25 @@ from __future__ import unicode_literals
 AUTHOR = u'Aleda'
 SITENAME = u'Aleda | Make Different'
 SITEURL = 'http://aleda.cn'
+#SITEURL = "http://cq01-rdqa-dev098.cq01.baidu.com:8000"
 
+DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'search', '404', 'base'))
 
-#MENUITEMS = (
-#        ('Categories', '/functions/categories.html'),
-#        ('Tags', '/functions/tags.html'),
-#        ('Archives', '/functions/archives.html'),
-#        ('Random Article', '/functions/random.html'),    
-#        )
-#
+MENUITEMS = (
+        ('Home', 'index.html'),
+        ('Categories', 'categories.html'),
+        ('Tags', 'tags.html'),
+        ('Archives', 'archives.html'),
+        ('Search', 'search.html'),
+        ('Random Article', 'random.html'),    
+        )
+
 PATH = 'content'
 
 TIMEZONE = 'Asia/Shanghai'
 DEFAULT_DATE_FORMAT = '%Y-%m-%d %H:%M'
 
-DEFAULT_LANG = u'cn'
+DEFAULT_LANG = u'zh'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -29,16 +33,19 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+USE_FOLDER_AS_CATEGORY = True
 
-ARTICLE_URL = '{category}/{slug}.html'
-ARTICLE_SAVE_AS = ARTICLE_URL
-PAGE_URL = '{slug}.html'
-PAGE_SAVE_AS = PAGE_URL
-CATEGORY_URL = '{slug}/index.html'
-CATEGORY_SAVE_AS = CATEGORY_URL
-TAG_URL = 'tag/{slug}.html'
-TAG_SAVE_AS = TAG_URL
-TAGS_SAVE_AS = 'tag/index.html'
+FILENAME_METADATA = '(?P<slug>.*)'
+
+ARTICLE_URL = 'article/{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = 'article/{date:%Y}/{date:%m}/{slug}/index.html'
+PAGE_URL = 'page/{date:%Y}/{date:%m}/{slug}/'
+PAGE_SAVE_AS = 'page/{date:%Y}/{date:%m}/{slug}/index.html'
+CATEGORY_URL = 'category/{date:%Y}/{date:%m}/{slug}/'
+CATEGORY_SAVE_AS = 'category/{date:%Y}/{date:%m}/{slug}/index.html'
+TAG_URL = 'tag/{date:%Y}/{date:%m}/{slug}/'
+TAG_SAVE_AS = 'tag/{date:%Y}/{date:%m}/{slug}/index.html'
+#TAGS_SAVE_AS = 'tag/index.html'
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -47,7 +54,8 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
+SOCIAL = (('Github', 'http://github.com/Aleda'),
+          (u'微博', 'http://weibo.com/u/2189437740'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
